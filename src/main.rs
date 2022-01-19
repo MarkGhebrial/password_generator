65ae1eb494d68713425f45c07d856f814fb2a368
MarkGhebrial <45022215+MarkGhebrial@users.noreply.github.com>
2022-01-19 17:05:39 +0000

    Obscure the password

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod password_generator;
-use password_generator::generate_password;
+use password_generator::*;
 
 use std::io;
 
 fn main() {
-    let (phrase, pass_len) = prompt_for_user_input();
+    let (phrase, password_length) = prompt_for_user_input();
 
-    let password = generate_password(&phrase);
+    let password = obscure_string(&generate_password(&phrase));
 
     println!("Your password: {}", password);
-    println!("Correct length passowrd: {}", repeat_string_for_length(&password, &pass_len));
+    println!("Correct length password: {}", repeat_string_for_length(&password, &password_length));
 }
 
 fn prompt_for_user_input() -> (String, u64) {
@@ -32,28 +32,3 @@ fn prompt_for_user_input() -> (String, u64) {
     (phrase, pass_len.unwrap())
 }
 
-/// Return a string consisting of the input string repeated
-/// for the specified number of chars.
-fn repeat_string_for_length(s: &String, length: &u64) -> String {
-    let mut out = String::new();
-
-    let mut string_iterator = s.chars().cycle();
-    for _ in 0..*length {
-        out.push(string_iterator.next().unwrap());
-    }
-
-    out
-}
-
-/// Unit test for string repetition
-#[test]
-fn string_repeat() {
-    assert_eq!(
-        repeat_string_for_length(&String::from("Mark"), &22),
-        String::from("MarkMarkMarkMarkMarkMa")
-    );
-    assert_eq!(
-        repeat_string_for_length(&String::from("Mark"), &2),
-        String::from("Ma")
-    );
-}
--- src/password_generator.rs
+++ src/password_generator.rs
@@ -6,7 +6,7 @@ pub fn generate_password(phrase: &String) -> String {
     for word in phrase.split(" ") { // Iterate over each substring, delimited with " "
         let word = word.trim(); // Trim off whitespace and newlines
 
-        // If the substring is a number, add all of it to the password
+        // If the entire substring is a number, add all of it to the password
         if let Ok(_) = word.parse::<f64>() {
             password.push_str(word);
             continue; // Move the next word
@@ -27,6 +27,42 @@ pub fn generate_password(phrase: &String) -> String {
     password // Return the password
 }
 
+/// "Obscure" a string by replacing some characters (like 't') with other
+/// simmilar characters (like '+')
+pub fn obscure_string(s: &String) -> String {
+    let mut out = String::new();
+    for (i, c) in s.chars().enumerate() {
+        if i % 2 == 1 { // Operate on every second character
+            out.push(match c {
+                't' => '+',
+                'o' => '0',
+                'i' => '1',
+                'I' => '1',
+                'B' => '&',
+                'l' => '|',
+                'A' => '^',
+                'k' => '<',
+                _ => c
+            });
+        } else { out.push(c) }
+    }
+
+    out
+}
+
+/// Return a string consisting of the input string repeated
+/// for the specified number of chars.
+pub fn repeat_string_for_length(s: &String, length: &u64) -> String {
+    let mut out = String::new();
+
+    let mut string_iterator = s.chars().cycle();
+    for _ in 0..*length {
+        out.push(string_iterator.next().unwrap());
+    }
+
+    out
+}
+
 /// Unit test for password generation
 #[test]
 fn password_gen() {
@@ -42,5 +78,27 @@ fn password_gen() {
     assert_eq!(
         generate_password(&phrase2),
         "MtLtDoM15"
+    );
+}
+
+/// Unit test for string obstruction
+#[test]
+fn string_obstruction() {
+    assert_eq!(
+        obscure_string(&String::from("tttoooiiiIIIBBBlllAAA")),
+        "tt+oo0ii1II1BB&ll|AA^"
+    );
+}
+
+/// Unit test for string repetition
+#[test]
+fn string_repeat() {
+    assert_eq!(
+        repeat_string_for_length(&String::from("Mark"), &22),
+        String::from("MarkMarkMarkMarkMarkMa")
+    );
+    assert_eq!(
+        repeat_string_for_length(&String::from("Mark"), &2),
+        String::from("Ma")
     );
 }
